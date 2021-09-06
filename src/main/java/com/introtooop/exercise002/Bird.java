6cdc6a990be3a29f4da31213787c4e02897b566d
mel4782gh <34482153+mel4782gh@users.noreply.github.com>
2021-09-06 14:28:31 +0100

    Initial commit

---
+++ src/main/java/com/introtooop/exercise002/Bird.java
@@ -0,0 +1,26 @@
+package main.java.com.introtooop.exercise002;
+
+public class Bird {
+
+    protected String name;
+    protected String color;
+
+    public Bird(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public void speak() {
+        System.out.println("My name is " + name + " and I am a " + color + " bird.");
+    }
+
+    public void fly() {
+        System.out.println("I'm " + name + " and I can fly high in the blue sky!");
+    }
+
+    public static void main(String[] args) {
+        Bird myBird = new Bird("Tweety", "Yellow");
+        myBird.speak();
+        myBird.fly();
+    }
+}
